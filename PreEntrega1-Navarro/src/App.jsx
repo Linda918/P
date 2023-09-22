282c3f032288d725f929d87d7f70ce3d1bb8da17
Linda NF <lindanavarro918@gmail.com>
2023-09-21 23:43:15 -0600

    primer commit

---
+++ PreEntrega1-Navarro/src/App.jsx
@@ -0,0 +1,14 @@
+import NavBar from "./components/header/NavBar";
+import ItemListContainer from "./components/header/ItemListContainer";
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <NavBar/>
+    <ItemListContainer/>
+    </>
+  )
+}
+
+export default App;
